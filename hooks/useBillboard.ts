ff6abbd809e599dafb406dc444dbbb3dc6294e2e
aniketparmar29 <aniketparmar068@gmail.com>
2023-05-10 12:22:59 +0530

    AS

--- hooks/useBillboard.ts
+++ hooks/useBillboard.ts
@@ -1,15 +1,17 @@
-import useSWR from "swr";
-import fetcher from "@/lib/fetcher";
+import useSwr from 'swr'
+import fetcher from '@/lib/fetcher';
 
-const useBillboard = () =>{
-    const {data,error,isLoading} = useSWR('/api/random',fetcher,{
-        revalidateIfStale:false,
-        revalidateOnFocus:false,
-        revalidateOnReconnect:false,
-    })
+const useBillboard = () => {
+  const { data, error, isLoading } = useSwr('/api/random', fetcher, { 
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+   });
+  return {
+    data,
+    error,
+    isLoading
+  }
+};
 
-    return{
-        data,error,isLoading
-    }
-}
 export default useBillboard;
